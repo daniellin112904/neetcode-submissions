538c4ad097e794bb520c64ee5f9b2f0e4165b3da
Daniel <daniellin112904@gmail.com>
2026-07-22 18:23:09 -0400

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.cpp
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        // pointer to beginning and ending chars
         int left = 0;
         int right = s.length() - 1;
         
         while (left < right) {
+            // move to next if not alnum
             while (!isalnum(s[left]) && left < right)
                 left++;
             while (!isalnum(s[right]) && left < right)
                 right--;
+            // if not equal, return false. tolower to convert chars to lowercase
             if (tolower(s[left]) != tolower(s[right]))
                 return false;
             else {
