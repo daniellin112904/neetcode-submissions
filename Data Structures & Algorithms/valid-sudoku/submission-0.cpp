9dfabbe27eb74831d4ab60690dafb7bfd76a5947
Daniel <daniellin112904@gmail.com>
2026-07-26 20:11:37 -0400

    Update: valid-sudoku - submission-0

--- Data Structures & Algorithms/valid-sudoku/submission-0.cpp
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-    unordered_map<int, unordered_set<char>> rows;
-    unordered_map<int, unordered_set<char>> cols;
-    unordered_map<int, unordered_set<char>> squares;
+        // map to store rows, columns and squares
+        unordered_map<int, unordered_set<char>> rows;
+        unordered_map<int, unordered_set<char>> cols;
+        unordered_map<int, unordered_set<char>> squares;
 
-    for (int r = 0; r < 9; r++) {
-        for (int c = 0; c < 9; c++) {
-            if (board[r][c] == '.')
-                continue;
+        // loop through whole board (9x9)
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                // skip if empty
+                if (board[r][c] == '.')
+                    continue;
 
-            char val = board[r][c];
-            int squareKey = (r / 3) * 3 + (c / 3);
+                // current value
+                char val = board[r][c];
+                // key calculation to store squares as unique int from 0-8 (see notion notes)
+                int squareKey = (r/3)*3 + (c/3);
 
-            if (rows[r].count(val) || cols[c].count(val) || squares[squareKey].count(val))
-                return false;
+                // return false if duplicate
+                if (rows[r].count(val) || cols[c].count(val) || squares[squareKey].count(val))
+                    return false;
 
-            rows[r].insert(val);
-            cols[c].insert(val);
-            squares[squareKey].insert(val);
+                // add value to map
+                rows[r].insert(val);
+                cols[c].insert(val);
+                squares[squareKey].insert(val);
+            }
         }
-    }
-    return true;
+        // true if no duplicate after whole loop
+        return true;
     }
 };
